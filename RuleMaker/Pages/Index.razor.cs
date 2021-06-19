845e700153470d80d94921978706b136893405c7
Paul Jenkins <paul@thewoodknight.com>
2021-06-19 22:42:21 +1000

    Init "Rule Maker" which will eventually take a gameID then have a wizard to produce a rough "starter" rule template (JSON + RUL)

---
+++ RuleMaker/Pages/Index.razor.cs
@@ -0,0 +1,38 @@
+﻿using BoardGamer.BoardGameGeek.BoardGameGeekXmlApi2;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using static BoardGamer.BoardGameGeek.BoardGameGeekXmlApi2.CollectionResponse;
+
+namespace RuleMaker.Pages
+{
+    public partial class Index
+    {
+
+        [Inject]
+        protected HttpClient Http { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            Fetch(220308);
+        }
+
+        public async Task Fetch(int id)
+        {
+            var _bggApiClient = new BoardGameGeekXmlApi2Client(Http);
+
+            ThingRequest request = new ThingRequest(new[] { id });
+            ThingResponse response = await _bggApiClient.GetThingAsync(request);
+            ThingResponse.Item game = response.Result.FirstOrDefault();
+
+            Console.WriteLine(game.Name);
+
+        }
+    }
+}
